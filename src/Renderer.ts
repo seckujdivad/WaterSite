708097262238d06065b30ba795f14e6c0a5f0b5a
seckujdivad <juckes1david@gmail.com>
2021-05-18 00:51:08 +0100

    Added new ShaderProgram class to manage WebGLPrograms

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -3,6 +3,7 @@ import WebGLDebugUtils from "webgl-debug";
 
 import App from "./App.jsx";
 import NarrowCanvas from "./HTMLCanvasTypes";
+import ShaderProgram from "./ShaderProgram";
 
 
 function WebGLErrorCallback(error: number, function_name: string)
@@ -16,18 +17,10 @@ class Renderer
 	
 	context: WebGL2RenderingContext;
 
-	shader_program: WebGLProgram;
 	vao: WebGLVertexArrayObject;
 	vbo: WebGLBuffer;
 
-	uniforms: {
-		transformation: WebGLUniformLocation,
-		perspective: WebGLUniformLocation,
-		waves_texture: WebGLUniformLocation,
-		sand_texture: WebGLUniformLocation
-	};
-
-	textures: [WebGLTexture, WebGLUniformLocation][];
+	shader_program: ShaderProgram;
 
 	constructor(app: App, context: WebGL2RenderingContext, vertex_shader_source: string, fragment_shader_source: string)
 	{
@@ -39,38 +32,7 @@ class Renderer
 			alert("Can't get WebGL context");
 		}
 
-		this.textures = [];
-
-		{
-			let vert_shader = gl.createShader(gl.VERTEX_SHADER);
-			gl.shaderSource(vert_shader, vertex_shader_source);
-			gl.compileShader(vert_shader);
-			if (!gl.getShaderParameter(vert_shader, gl.COMPILE_STATUS))
-			{
-				alert(gl.getShaderInfoLog(vert_shader));
-			}
-
-			let frag_shader = gl.createShader(gl.FRAGMENT_SHADER);
-			gl.shaderSource(frag_shader, fragment_shader_source);
-			gl.compileShader(frag_shader);
-			if (!gl.getShaderParameter(frag_shader, gl.COMPILE_STATUS))
-			{
-				alert(gl.getShaderInfoLog(frag_shader));
-			}
-
-			this.shader_program = gl.createProgram();
-			gl.attachShader(this.shader_program, vert_shader);
-			gl.attachShader(this.shader_program, frag_shader);
-			gl.linkProgram(this.shader_program);
-
-			if (!gl.getProgramParameter(this.shader_program, gl.LINK_STATUS))
-			{
-				alert(gl.getProgramInfoLog(this.shader_program));
-			}
-
-			gl.deleteShader(vert_shader);
-			gl.deleteShader(frag_shader);
-		}
+		this.shader_program = new ShaderProgram(context, vertex_shader_source, fragment_shader_source);
 
 		let tri_data = [
 			[
@@ -131,30 +93,12 @@ class Renderer
 		gl.vertexAttribPointer(2, 3, gl.FLOAT, false, 11 * SIZEOF_FLOAT, 5 * SIZEOF_FLOAT);
 		gl.vertexAttribPointer(3, 3, gl.FLOAT, false, 11 * SIZEOF_FLOAT, 8 * SIZEOF_FLOAT);
 
-		this.uniforms = {
-			transformation: gl.getUniformLocation(this.shader_program, "transformation"),
-			perspective: gl.getUniformLocation(this.shader_program, "perspective"),
-			waves_texture: gl.getUniformLocation(this.shader_program, "wavesTexture"),
-			sand_texture: gl.getUniformLocation(this.shader_program, "sandTexture")
-		};
+		this.shader_program.AddUniform("transformation");
+		this.shader_program.AddUniform("perspective");
 
-		let waves_texture = this.LoadTexture("./SeaWavesB_N.jpg");
-		this.textures.push([waves_texture, this.uniforms.waves_texture]);
-
-		let sand_texture = this.LoadTexture("./seamless_desert_sand_texture_by_hhh316_d311qn7-fullview.jpg");
-		this.textures.push([sand_texture, this.uniforms.sand_texture]);
-
-		gl.useProgram(this.shader_program);
-		for (let i = 0; i < this.textures.length; i++)
-		{
-			const texture = this.textures[i][0];
-			const uniform = this.textures[i][1];
-			gl.activeTexture(gl.TEXTURE0 + i + 1);
-			gl.bindTexture(gl.TEXTURE_2D, texture);
-			gl.uniform1i(uniform, i + 1);
-		}
-		gl.activeTexture(gl.TEXTURE0);
-	}
+		this.shader_program.LoadTexture("wavesTexture", "./SeaWavesB_N.jpg");
+		this.shader_program.LoadTexture("sandTexture", "./seamless_desert_sand_texture_by_hhh316_d311qn7-fullview.jpg");
+	};
 
 	Render()
 	{
@@ -167,16 +111,16 @@ class Renderer
 
 		gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-		gl.useProgram(this.shader_program);
+		this.shader_program.Use();
 
 		//set up uniforms
 		let perspective = mat4.identity(mat4.create());
 		mat4.perspective(perspective, Math.PI / 4, gl.canvas.width / gl.canvas.height, 0.1, 100);
-		gl.uniformMatrix4fv(this.uniforms.perspective, false, perspective);
+		gl.uniformMatrix4fv(this.shader_program.uniforms.get("perspective"), false, perspective);
 
 		let transformation = mat4.identity(mat4.create());
 		mat4.translate(transformation, transformation, vec3.fromValues(0, 0, -1));
-		gl.uniformMatrix4fv(this.uniforms.transformation, false, transformation);
+		gl.uniformMatrix4fv(this.shader_program.uniforms.get("transformation"), false, transformation);
 		
 		//perform render
 		gl.clearColor(1, 1, 1, 1);
@@ -184,43 +128,16 @@ class Renderer
 		
 		gl.bindVertexArray(this.vao);
 		gl.drawArrays(gl.TRIANGLES, 0, 6);
-	}
+	};
 
 	Destroy()
 	{
 		const gl = this.context;
-		gl.deleteProgram(this.shader_program);
 		gl.deleteVertexArray(this.vao);
 		gl.deleteBuffer(this.vbo);
 
-		this.textures.forEach(gl.deleteTexture);
-	}
-
-	//https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Using_textures_in_WebGL
-	LoadTexture(url: string): WebGLTexture
-	{
-		const gl = this.context;
-		const texture = gl.createTexture();
-		gl.bindTexture(gl.TEXTURE_2D, texture);
-
-		//default initialise to 1x1 black texture
-		const pixel = new Uint8Array([0, 0, 0, 255]);
-		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixel);
-
-		const image = new Image();
-		image.onload = function ()
-		{
-			gl.bindTexture(gl.TEXTURE_2D, texture);
-			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-		};
-		image.src = url;
-
-		return texture;
-	}
-}
+		this.shader_program.Destroy();
+	};
+};
 
 export default Renderer;
---
+++ src/ShaderProgram.ts
@@ -0,0 +1,117 @@
+class ShaderProgram
+{
+	context: WebGL2RenderingContext;
+
+	obj: WebGLProgram;
+
+	uniforms: Map<string, WebGLUniformLocation>;
+	textures: Map<string, WebGLTexture>;
+
+	constructor(context: WebGL2RenderingContext, vertex_shader_source: string, fragment_shader_source: string)
+	{
+		this.context = context;
+		const gl = this.context;
+
+		this.uniforms = new Map();
+		this.textures = new Map();
+
+		let vert_shader = gl.createShader(gl.VERTEX_SHADER);
+		gl.shaderSource(vert_shader, vertex_shader_source);
+		gl.compileShader(vert_shader);
+		if (!gl.getShaderParameter(vert_shader, gl.COMPILE_STATUS))
+		{
+			alert(gl.getShaderInfoLog(vert_shader));
+		}
+
+		let frag_shader = gl.createShader(gl.FRAGMENT_SHADER);
+		gl.shaderSource(frag_shader, fragment_shader_source);
+		gl.compileShader(frag_shader);
+		if (!gl.getShaderParameter(frag_shader, gl.COMPILE_STATUS))
+		{
+			alert(gl.getShaderInfoLog(frag_shader));
+		}
+
+		this.obj = gl.createProgram();
+		gl.attachShader(this.obj, vert_shader);
+		gl.attachShader(this.obj, frag_shader);
+		gl.linkProgram(this.obj);
+
+		if (!gl.getProgramParameter(this.obj, gl.LINK_STATUS))
+		{
+			alert(gl.getProgramInfoLog(this.obj));
+		}
+
+		gl.deleteShader(vert_shader);
+		gl.deleteShader(frag_shader);
+	};
+
+	Destroy()
+	{
+		const gl = this.context;
+		gl.deleteProgram(this.obj);
+		for (const [name, texture] of this.textures)
+		{
+			gl.deleteTexture(texture);
+		}
+	};
+
+	AddUniform(name: string)
+	{
+		const gl = this.context;
+		this.uniforms.set(name, gl.getUniformLocation(this.obj, name));
+	}
+
+	LoadTexture(name: string, url: string): WebGLTexture
+	{
+		const gl = this.context;
+		const texture = gl.createTexture();
+		gl.bindTexture(gl.TEXTURE_2D, texture);
+
+		//default initialise to 1x1 black texture
+		const pixel = new Uint8Array([0, 0, 0, 255]);
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixel);
+
+		const image = new Image();
+		image.onload = function ()
+		{
+			gl.bindTexture(gl.TEXTURE_2D, texture);
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+		};
+		image.src = url;
+
+		this.textures.set(name, texture);
+		this.AddUniform(name);
+
+		this.SetTextures();
+
+		return texture;
+	};
+
+	SetTextures()
+	{
+		const gl = this.context;
+
+		gl.useProgram(this.obj);
+		let i = 0;
+		for (const [name, texture] of this.textures)
+		{
+			const uniform: WebGLUniformLocation = this.uniforms.get(name);
+			gl.activeTexture(gl.TEXTURE0 + i + 1);
+			gl.bindTexture(gl.TEXTURE_2D, texture);
+			gl.uniform1i(uniform, i + 1);
+			i += 1;
+		};
+		gl.activeTexture(gl.TEXTURE0);
+	};
+
+	Use()
+	{
+		this.context.useProgram(this.obj);
+	};
+};
+
+export default ShaderProgram;
