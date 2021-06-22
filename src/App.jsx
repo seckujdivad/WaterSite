c1fc0b589737ddf4b7bc812bddd647e2ed222b51
seckujdivad <juckes1david@gmail.com>
2021-06-22 18:42:01 +0100

    Moved glcanvas inside App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import styles from "./App.module.css";
 import SceneLayer from "./SceneLayer.jsx";
+import Renderer from "./Renderer";
 
 
 class App extends React.Component
@@ -20,8 +21,30 @@ class App extends React.Component
 					transparency: 0.5,
 					colour: "#6060FF"
 				}
-			]
+			],
+
+			canvas_ref: React.createRef(),
+			get canvas()
+			{
+				return this.canvas_ref.current;
+			}
 		};
+
+		const createRenderer = function (window, shaders)
+		{
+			const context = this.state.canvas.getContext("webgl2");
+			const renderer = new Renderer(this, context, shaders.vertex, shaders.fragment);
+
+			const OnClose = function(renderer, event)
+			{
+				renderer.Destroy();
+			}
+			window.onclose = OnClose.bind(this, renderer);
+
+			window.setInterval(renderer.Render.bind(renderer), 10); //10 ms
+		}
+
+		queryShaders().then(createRenderer.bind(this, window));
 	}
 
 	render()
@@ -30,13 +53,17 @@ class App extends React.Component
 		for (let i = 0; i < this.state.layers.length; i++)
 		{
 			const layer = this.state.layers[i];
-			layer_components.push(<SceneLayer key={i} row={i} numRows={this.state.layers.length} onMovePressed={this.moveRow.bind(this, i)}
-				transparency={layer.transparency} onTransparencyChange={this.updateLayerTransparency.bind(this, i)}
-				colour={layer.colour} onColourChange={this.updateLayerColour.bind(this, i)}
-				></SceneLayer>)
+			layer_components.push(
+					<SceneLayer key={i} row={i} numRows={this.state.layers.length} onMovePressed={this.moveRow.bind(this, i)}
+					transparency={layer.transparency} onTransparencyChange={this.updateLayerTransparency.bind(this, i)}
+					colour={layer.colour} onColourChange={this.updateLayerColour.bind(this, i)} />
+				);
 		}
 
-		return <div className={styles.App}>{layer_components}</div>;
+		return <>
+				<div className={styles.App}>{layer_components}</div>
+				<canvas className={styles.glcanvas} ref={this.state.canvas_ref} />
+			</>;
 	}
 
 	updateLayerTransparency(index, transparency)
@@ -75,5 +102,11 @@ class App extends React.Component
 	}
 };
 
+async function queryShaders()
+{
+	let [vertex_shader_req, fragment_shader_req] = await Promise.all([fetch("water.vert"), fetch("water.frag")]);
+	let [vertex_shader, fragment_shader] = await Promise.all([vertex_shader_req.text(), fragment_shader_req.text()]);
+	return {vertex: vertex_shader, fragment: fragment_shader};
+}
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,34 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App.jsx";
-import Renderer from "./Renderer";
 
-async function QueryShaders()
-{
-	let [vertex_shader_req, fragment_shader_req] = await Promise.all([fetch("water.vert"), fetch("water.frag")]);
-	let [vertex_shader, fragment_shader] = await Promise.all([vertex_shader_req.text(), fragment_shader_req.text()]);
-	return {vertex: vertex_shader, fragment: fragment_shader};
-}
 
-let reference = React.createRef();
-ReactDOM.render(<App ref={reference} />, document.getElementById("root"));
-const app = reference.current;
-
-//webgl
-const canvas = document.getElementById("glcanvas");
-const context = canvas.getContext("webgl2");
-
-const Initialiser = function (app, context, window, shaders)
-{
-	const renderer = new Renderer(app, context, shaders.vertex, shaders.fragment);
-
-	const OnClose = function(renderer, event)
-	{
-		renderer.Destroy();
-	}
-	window.onclose = OnClose.bind(this, renderer);
-
-	window.setInterval(renderer.Render.bind(renderer), 10); //10 ms
-}
-
-QueryShaders().then(Initialiser.bind(this, app, context, window));
+ReactDOM.render(<App />, document.getElementById("root"));
