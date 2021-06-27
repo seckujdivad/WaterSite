47231b45fc7540568435fd76febfc2dd9b04a650
seckujdivad <juckes1david@gmail.com>
2021-06-27 17:41:22 +0100

    Fixed camera transformations

--- src/rendering/Camera.ts
+++ src/rendering/Camera.ts
@@ -3,29 +3,28 @@ import {mat4, vec3} from "gl-matrix";
 
 class Camera
 {
-    position: vec3;
-    rotation: vec3;
+	position: vec3;
+	rotation: vec3;
 
-    constructor(position: vec3, rotation: vec3)
-    {
-        this.position = position;
-        this.rotation = rotation;
-    }
+	constructor(position: vec3, rotation: vec3)
+	{
+		this.position = position;
+		this.rotation = rotation;
+	}
 
-    getTransformation(): mat4
-    {
-        let transformation: mat4 = mat4.create();
+	getTransformation(): mat4
+	{
+		let transformation: mat4 = mat4.create();
 
-        let translate_vec = vec3.create();
-        vec3.subtract(translate_vec, vec3.create(), this.position);
-        mat4.translate(transformation, transformation, translate_vec);
+		mat4.rotateX(transformation, transformation, 0 - this.rotation[0]);
+		mat4.rotateY(transformation, transformation, 0 - this.rotation[1]);
+		mat4.rotateZ(transformation, transformation, 0 - this.rotation[2]);
 
-        mat4.rotateX(transformation, transformation, 0 - this.rotation[0]);
-        mat4.rotateY(transformation, transformation, 0 - this.rotation[1]);
-        mat4.rotateZ(transformation, transformation, 0 - this.rotation[2]);
-
-        return transformation;
-    }
+		let translate_vec = vec3.create();
+		vec3.subtract(translate_vec, vec3.create(), this.position);
+		mat4.translate(transformation, transformation, translate_vec);
+		return transformation;
+	}
 }
 
 export default Camera;
