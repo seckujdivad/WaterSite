d146dde8f21da18ed00002415f43c0b267359fac
seckujdivad <juckes1david@gmail.com>
2021-07-20 02:10:30 +0100

    Split rotation matrix generation into separate function

--- src/rendering/TranslateLocally.ts
+++ src/rendering/TranslateLocally.ts
@@ -1,12 +1,18 @@
-import {vec3, mat4, mat3} from "gl-matrix";
+import {vec3, mat4} from "gl-matrix";
 
 //in place rotates 'source' by 'rotation' radians
 function rotateVector(source: vec3, rotation: vec3): vec3
 {
-	let transformation = mat4.fromXRotation(mat4.create(), rotation[0]);
-	mat4.rotateY(transformation, transformation, rotation[1]);
-	mat4.rotateZ(transformation, transformation, rotation[2]);
-	return vec3.transformMat4(source, source, transformation);
+	return vec3.transformMat4(source, source, createRotation(rotation));
 }
 
-export default rotateVector;
+function createRotation(rotation: vec3)
+{
+	let transformation = mat4.create();
+	mat4.rotateX(transformation, transformation, rotation[0]);
+	mat4.rotateY(transformation, transformation, rotation[1]);
+	mat4.rotateZ(transformation, transformation, rotation[2]);
+	return transformation;
+}
+
+export {rotateVector as default, createRotation};
