5585d40f2483766db2d2f1bfcce4bc3b47aa161b
seckujdivad <juckes1david@gmail.com>
2021-08-05 21:32:09 +0100

    Changed SkyboxMaterial to use createTextureCubemap

--- src/rendering/material/skybox/SkyboxMaterial.ts
+++ src/rendering/material/skybox/SkyboxMaterial.ts
@@ -2,14 +2,14 @@ import {vec3} from "gl-matrix";
 
 import IMaterial from "../IMaterial";
 import MaterialIdentifier from "../MaterialIdentifier";
-import {TextureCubemap, TextureType} from "../../texture/Texture";
+import {TextureCubemap, createTextureCubemap} from "../../texture/Texture";
 import SkyboxRenderer from "./SkyboxRenderer";
 
 class SkyboxMaterial implements IMaterial
 {
 	skybox_cubemap: TextureCubemap;
 
-	constructor(skybox_cubemap: TextureCubemap = {type: TextureType.TextureCubemap, data: vec3.fromValues(0, 0, 0)})
+	constructor(skybox_cubemap = createTextureCubemap(vec3.fromValues(0, 0, 0)))
 	{
 		this.skybox_cubemap = skybox_cubemap;
 	}
